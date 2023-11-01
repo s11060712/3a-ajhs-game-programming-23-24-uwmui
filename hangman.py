03c513722cf59c5b8294dbe860e7bfb0e9d0d884
uwmui <aidanjmoody32061@gmail.com>
2023-11-01 11:40:15 -0400

    Update hangman.py

---
+++ hangman.py
@@ -0,0 +1,4 @@
+wordList = 'Fortune Temperance Justice Magician Judgement Devil Death Jester Aeon World Star Sun Moon Hermit Hierophant Chariot Priestess Emperor Empress Lovers Strength'.split()
+print(wordList)
+
+print(wordList[0])
