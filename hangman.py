48f4a0622b89b600d8fe885c4694cfaf363b259e
uwmui <aidanjmoody32061@gmail.com>
2023-11-01 12:58:11 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -1,3 +1,4 @@
+import random
 wordList = 'Fortune Temperance Justice Magician Judgement Devil Death Jester Aeon World Star Sun Moon Hermit Hierophant Chariot Priestess Emperor Empress Lovers Strength'.split()
 # .split() will split a string into seperate elements, by default based on SPACE
 
@@ -46,7 +47,13 @@ HANGMAN_BOARD = ['''
           |       
          ====''']                     
 
+def getRandomWord(wordList):
+    wordIndex = random.randint(0, len(wordList) -1)
+    # len(listName) -1 IS MOST COMMON WAY TO PREVENT INDEX OUT OF RANGE
+    # print(wordIndex)
+    return wordList[wordIndex]
+
 i = 0
-while i < len(HANGMAN_BOARD):
-    print(HANGMAN_BOARD[i])
-    i += 1
+while i < 500000:
+     getRandomWord(wordList)
+     i += 1
