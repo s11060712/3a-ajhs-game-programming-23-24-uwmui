0ecd37983cdeedc2aaa4c8c4859459a288b7efbc
uwmui <aidanjmoody32061@gmail.com>
2023-11-03 13:14:02 -0400

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -53,9 +53,58 @@ def getRandomWord(wordList):
     # print(wordIndex)
     return wordList[wordIndex]
 
-def displayBoard(incorrectLetters, correctLetters, secretWord):
-    print(HANGMAN_BOARD[len(incorrectLetters)])
+def displayBoard(missedLetters, correctLetters, secretWord):
+    print(HANGMAN_BOARD[len(missedLetters)])
     print()
+
+    # Display the Missed Letters
+    print('Missed Letters:', end = '  ')
+    for letter in missedLetters:
+       print(letter, end = ' ')
+       print()
+    
+    
+    blanks = '_' * len(secretWord)
+    for i in range(len(secretWord)):
+           if secretWord[i] in correctLetters:
+                blanks = blanks[:i] + secretWord[i] + blanks[i+1]
+                # The : character is used to SLICE strings into pieces.
+                # [i] means slice from the start until index i
+                # [i+1:] means slice from i+1 until the END
+                # [startingPoint:endingPoint]
+
+    for letter in blanks:
+          print(letter, end = ' ')
+          print() 
+    
+
+def getGuess(alreadyGuessed):
+     # Only allow: single character, A-Z only, not guessed already.
+     while True: # default 'infinite' loop
+          print('Please guess a letter and press Enter.')
+          guess = input()
+          guess = guess.lower()
+          if len(guess) != 1:
+               print('Please enter a single letter.\n')
+          elif guess not in 'abcdefghijklmnopqrstuvwxyz':
+            print('Please enter an English letter only.')
+          elif guess in alreadyGuessed:
+            print('Already guessed. Please guess a different letter!')
+          else:
+              return guess
+
+def playAgain():
+    print('Do you want to play again? Yes or No, then press Enter.')
+    return input().lower().startswith('y')
+
+
+
+
+
+
+
+
+
 
 # i = 0
 # while i < 500000:
